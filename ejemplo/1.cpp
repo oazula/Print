d567b8660dd94c65c5c24a9f2e9b76a8b724d72a
oscar <oazula@unal.edu.co>
2020-09-24 16:12:53 +0000

    3

--- ejemplo/1.cpp
+++ ejemplo/1.cpp
@@ -4,22 +4,7 @@
 #include <functional>
 
 
-double f(double x);
-double deriv2_exact(double x1);
-double deriv2a(double f1,double x1,double dx);
-double deriv2b(double f1,double x1,double dx);
-
-
-int main(int argc, char **argv)
-{
-    std::cout.precision(16); std::cout.setf(std::ios::scientific);
-    const double x = std::atof(argv[1]);
-    double h = 0;
-    double delta1, delta2; // percentual diffs
-    for (int ii = 1; ii <= 16; ++ii) {
-        double h = M_PI*std::pow(10.0, -ii);
-        delta1 = std::fabs(1.0 - deriv2a(f(x), x, h)/deriv2_exact(x));
-        delta2 = std::fabs(1.0 - deriv2b(f(x), x, h)/deriv2_exact(x));
+d
         std::cout << h << " \t" << delta1 << "\t" << delta2 << "\n";
     }
   
