738a6a8a963f2935bdc5f249ab510caa351e7dc9
oscar <oazula@unal.edu.co>
2020-09-24 16:10:45 +0000

    2

--- ejemplo/1.cpp
+++ ejemplo/1.cpp
@@ -1,65 +1,48 @@
 #include <iostream>
-#include <vector>
+#include <cstdlib>
 #include <cmath>
 #include <functional>
-#include <cstdlib>
 
-using fptr  = double(double);
-using fptr2 = double(fptr, double, double, int);
 
 double f(double x);
-double trapezoid(fptr fun, double a, double b, int n);
-double richardson(fptr2 alg, fptr func, double a, double b, double eps);
+double deriv2_exact(double x1);
+double deriv2a(double f1,double x1,double dx);
+double deriv2b(double f1,double x1,double dx);
+
 
 int main(int argc, char **argv)
 {
-    std::cout.precision(15); std::cout.setf(std::ios::scientific);
-    const double eps = std::atof(argv[1]);
-    std::cout << richardson(trapezoid, f, 0, 1, eps) << "\n";
-
-  return 0;
+    std::cout.precision(16); std::cout.setf(std::ios::scientific);
+    const double x = std::atof(argv[1]);
+    double h = 0;
+    double delta1, delta2; // percentual diffs
+    for (int ii = 1; ii <= 16; ++ii) {
+        double h = M_PI*std::pow(10.0, -ii);
+        delta1 = std::fabs(1.0 - deriv2a(f(x), x, h)/deriv2_exact(x));
+        delta2 = std::fabs(1.0 - deriv2b(f(x), x, h)/deriv2_exact(x));
+        std::cout << h << " \t" << delta1 << "\t" << delta2 << "\n";
+    }
+  
+    return 0;
 }
 
+double deriv2a(double f1,double x1,double dx)
+{
+return ((f(x1+dx)-f1)-(f1-f(x1-dx)))/(dx*dx);
+}
 
+double deriv2b(double f1,double x1,double dx)
+{
+return (f(x1+dx)+f(x1-dx)-2*f1)/(dx*dx);
 
+}
+
+double deriv2_exact(double x1)
+{
+  return ((-2*std::sin(x1))-(x1*std::cos(x1)));
+}
 
 double f(double x)
 {
-  return (1.2-x)*(1-std::exp(20*(x-1)))*std::pow(x, 0.1);
-  
-}
-
-double trapezoid(fptr fun, double a, double b, int n)
-{
-      const double h = (b-a)/n;
-    double suma = 0.5*(fun(a) + fun(b));
-    for(int ii = 1; ii <= n-1; ++ii) {
-        double xi = a + ii*h;
-        suma += fun(xi);
-    }
-    suma *= h;
-    return suma;
-}
-
-double richardson(fptr2 alg, fptr func, double a, double b, double eps)
-{
-  double val1= 0;
-  double val2= 0;
-  int n=0;
-  double diff =1;
-
- for (n=1;diff>eps; n=2*n){
-
-    val1=(4*alg(func, a, b, 2*n) - alg(func, a, b, n))/3;
-    val2=(4*alg(func, a, b, 4*n) - alg(func, a, b, 2*n))/3;
-
-    diff= std::fabs(1- val1/val2);
-    
-  }
-  
-std::cout<< n << "\n";
-std::cout<<"diff: "<<diff << "\n";
-
-
-    return val1;
+    return x*std::cos(x);
 }
